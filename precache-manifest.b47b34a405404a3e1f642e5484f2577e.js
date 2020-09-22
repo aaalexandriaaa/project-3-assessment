1767eaf70ff34de84e5c2b11891b8be559dde1b4
aaalexandriaaa <67809768+aaalexandriaaa@users.noreply.github.com>
2020-09-22 14:05:43 -0400

    Updates

---
+++ precache-manifest.b47b34a405404a3e1f642e5484f2577e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd2b6ef57fa0ec77b1c32c39f87c47ae",
+    "url": "/git%20branch%20-M%20master/index.html"
+  },
+  {
+    "revision": "84ba5b436918aa104479",
+    "url": "/git%20branch%20-M%20master/static/css/main.e3a6c5a6.chunk.css"
+  },
+  {
+    "revision": "d87a5c490be35129f08f",
+    "url": "/git%20branch%20-M%20master/static/js/2.2553baad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/git%20branch%20-M%20master/static/js/2.2553baad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84ba5b436918aa104479",
+    "url": "/git%20branch%20-M%20master/static/js/main.3420b1f5.chunk.js"
+  },
+  {
+    "revision": "2a968e4b9960faf52229",
+    "url": "/git%20branch%20-M%20master/static/js/runtime-main.62ce93a7.js"
+  }
+]);
